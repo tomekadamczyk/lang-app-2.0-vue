e7504609032184f4b628abf6e15240b7333a89ec
Tomek A <tomus.adamczyk@gmail.com>
2019-11-16 11:52:12 +0100

    Layout component added

---
+++ lang-vue/src/components/Layout/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="layout">
+        <slot/>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Layout',
+}
+</script>
+
+<style lang="scss" scoped>
+    .layout {
+        width: calc(100% - 250px);
+    height: 100vh;
+    }
+</style>
