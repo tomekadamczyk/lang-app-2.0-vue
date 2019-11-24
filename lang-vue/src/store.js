66ade8a81cfdae0fec17dcaa2dd2bcf96b961fbd
Tomek A <tomus.adamczyk@gmail.com>
2019-11-24 18:13:35 +0100

    Set vuex state

--- lang-vue/src/store.js
+++ lang-vue/src/store.js
@@ -5,7 +5,50 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    defaultLanguage: 'czech',
+    dictionary: [],
+    languages: [
+      {
+        name: 'czech',
+        cards: [
+          { value: 'word', translation: 'slowo' },
+          { value: 'tramwaj', translation: 'tramvaj' },
+          { value: 'spodnie', translation: 'kalhoty' },
+          { value: 'kurtka', translation: 'bunda' },
+          { value: 'góry', translation: 'hory' },
+        ],
+      },
+      {
+        name: 'english',
+        cards: [
+          { value: 'word', translation: 'słowo' },
+          { value: 'tramwaj', translation: 'train' },
+          { value: 'spodnie', translation: 'pants' },
+          { value: 'kurtka', translation: 'jacket' },
+          { value: 'góry', translation: 'mountains' },
+        ],
+      },
+      {
+        name: 'german',
+        cards: [
+          { value: 'word', translation: 'slowen' },
+          { value: 'tramwaj', translation: 'tramvajen' },
+          { value: 'spodnie', translation: 'kalhoten' },
+          { value: 'kurtka', translation: 'kurtken' },
+          { value: 'góry', translation: 'góren' },
+        ],
+      },
+      {
+        name: 'italian',
+        cards: [
+          { value: 'word', translation: 'slovacoo' },
+          { value: 'tramwaj', translation: 'tramvajo' },
+          { value: 'spodnie', translation: 'spodniento' },
+          { value: 'kurtka', translation: 'kurtkando' },
+          { value: 'góry', translation: 'górento' },
+        ],
+      },
+    ],
   },
   mutations: {
 
