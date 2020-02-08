a3f523561c5eb93d30c5d662161aef2d4e5cd0df
Tomek A <tomus.adamczyk@gmail.com>
2020-02-08 22:51:15 +0100

    wordview component

---
+++ lang-vue/src/components/WordView/WordView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div class="word__name">word name</div>
+        <div class="word__cases">
+            <table>
+                <thead>
+                    <th></th>
+                    <th>Singular</th>
+                    <th>Plural</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mianownik</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Dopełniacz</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Celownik</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Biernik</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Wołacz</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Miejscownik</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <td>Narzędnik</td>
+                        <td>3</td>
+                        <td>3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WordView',
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ lang-vue/src/views/WordView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Word</h1>
+    <WordView />
+  </div>
+</template>
+
+<script>
+import WordView from '../components/WordView/WordView.vue';
+
+export default {
+  components: {
+    WordView,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
