ca11589bb34b834b5e02183e666626a3ef39bbdd
Tomek A <tomus.adamczyk@gmail.com>
2020-02-08 22:50:49 +0100

    remove text center

--- lang-vue/src/App.vue
+++ lang-vue/src/App.vue
@@ -30,11 +30,10 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   display: flex;
   flex-direction: column;
-  
+
     @include media-screen(tablet-up) {
       flex-direction: row;
     }
