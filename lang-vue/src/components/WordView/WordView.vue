2b6978a158da4dce11312e5090b135beaafb05a7
Tomek A <tomus.adamczyk@gmail.com>
2020-02-23 20:24:37 +0100

    generate single word view by word id

--- lang-vue/src/components/WordView/WordView.vue
+++ lang-vue/src/components/WordView/WordView.vue
@@ -1,58 +1,78 @@
 <template>
     <div>
-        <div class="word__name">word name</div>
-        <div class="word__cases">
-            <table>
-                <thead>
-                    <th></th>
-                    <th>Singular</th>
-                    <th>Plural</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Mianownik</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Dopełniacz</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Celownik</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Biernik</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Wołacz</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Miejscownik</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                    <tr>
-                        <td>Narzędnik</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <div class="word__name">{{ wordName }}</div>
+        <VTable
+            v-bind:tableDataArray="wordPageData"
+            v-bind:dataToHide="propsToHide"
+            v-bind:checkRouter="checkRouter"
+        />
     </div>
 </template>
 
 <script>
+import VTable from '../UI/Table/VTable.vue';
+import store from '../../store';
+
 export default {
   name: 'WordView',
+  components: {
+    VTable,
+  },
+  data() {
+    return {
+      sharedState: store.state,
+      propsToHide: [],
+      checkRouter: false,
+      wordId: null,
+      wordTable: [],
+      selectedCard: {},
+      wordName: '',
+      grammaticalCasesArray: [],
+    };
+  },
+  methods: {
+    getWordId() {
+      this.wordId = this.$route.params.wordId;
+    },
+    concatDataToHide() {
+      store.state.partsOfSpeech.forEach((_, index) => {
+        this.propsToHide.push(index + 1);
+      });
+    },
+    generateWordData() {
+      Object.keys(this.grammaticalCases).forEach((caseData) => {
+        this.grammaticalCasesArray.push(this.grammaticalCases[caseData].value);
+      });
+      Object.keys(this.selectedCard).forEach((key) => {
+        if (key === 'value') {
+          this.wordName = this.selectedCard[key];
+        }
+        if (this.grammaticalCasesArray.includes(key)) {
+          this.wordTable.push(this.selectedCard[key]);
+        }
+      });
+    },
+  },
+  created() {
+    this.getWordId();
+    store.state.languages[0].cards.forEach((card) => {
+      Object.keys(card).forEach((cardData) => {
+        if (card[cardData] === Number(this.wordId)) {
+          this.selectedCard = card;
+        }
+      });
+    });
+    this.generateWordData();
+    this.concatDataToHide();
+  },
+  computed: {
+    grammaticalCases() {
+      return this.$store.state.grammaticalCases;
+    },
+    wordPageData() {
+      return this.$store.state.wordPageData;
+    },
+  },
 };
 </script>
 
