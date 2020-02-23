88f5bd52657874c1993dffa88eaa370d730e128c
Tomek A <tomus.adamczyk@gmail.com>
2020-02-23 21:26:34 +0100

    Dropdown with choosing language

--- lang-vue/src/components/UI/Dropdown/Dropdown.vue
+++ lang-vue/src/components/UI/Dropdown/Dropdown.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <div class="select__selected-value">Chosen language: <span>{{ selected }}</span></div>
-        <select 
+        <select
             v-model="selected"
             v-on:change="changeValue"
             class="select"
         >
-            <Option 
+            <Option
                 v-for="(option, index) in options"
-                v-bind:key="index" 
+                v-bind:key="index"
                 v-bind:value="option.value"
             >{{option.name}}</Option>
         </select>
@@ -16,30 +16,26 @@
 </template>
 
 <script>
-import DropdownOption from './DropdownOption/DropdownOption.vue';
 
 export default {
-    name: 'Dropdown',
-    components: {
-        DropdownOption,
+  name: 'Dropdown',
+  data() {
+    return {
+      selected: 'czech',
+      options: [
+        { id: 0, name: 'Czech', value: 'czech' },
+        { id: 1, name: 'English', value: 'english' },
+        { id: 2, name: 'German', value: 'german' },
+        { id: 3, name: 'Italian', value: 'italian' },
+      ],
+    };
+  },
+  methods: {
+    changeValue() {
+      this.$emit('selectChange', this.selected);
     },
-    data() {
-        return {
-            selected: 'czech',
-            options: [
-                { id: 0, name: 'Czech', value: 'czech' },
-                { id: 1, name: 'English', value: 'english' },
-                { id: 2, name: 'German', value: 'german' },
-                { id: 3, name: 'Italian', value: 'italian' },
-            ],
-        };
-    },
-    methods: {
-        changeValue() {
-            this.$emit('selectChange', this.selected)
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
