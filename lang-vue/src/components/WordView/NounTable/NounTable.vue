9e0d9ad8d3fced036802f14880bd839fb8c328c0
Tomek A <tomus.adamczyk@gmail.com>
2020-02-24 17:59:56 +0100

    divide word tables by noun component

--- lang-vue/src/components/WordView/NounTable/NounTable.vue
+++ lang-vue/src/components/WordView/NounTable/NounTable.vue
@@ -1,6 +1,5 @@
 <template>
     <div style="width: 400px;">
-        <div class="word__name"><strong>{{ wordName }}</strong> - {{ wordTranslation }}</div>
         <VTable
             v-bind:tableDataArray="preparedData"
             v-bind:dataToHide="propsToHide"
@@ -11,7 +10,6 @@
 
 <script>
 import VTable from '../../UI/Table/VTable.vue';
-import store from '../../../store';
 
 export default {
   name: 'WordView',
@@ -22,44 +20,24 @@ export default {
     counting: {
       type: String,
     },
+    selectedCard: {
+      type: Object,
+    },
+    grammaticalCasesArray: {
+      type: Array,
+    },
   },
   data() {
     return {
-      sharedState: store.state,
       propsToHide: [],
       checkRouter: false,
-      wordId: null,
-      wordTable: [],
-      selectedCard: {},
-      wordName: '',
-      wordTranslation: '',
       preparedData: [],
-      grammaticalCasesArray: [],
     };
   },
   methods: {
-    getWordId() {
-      this.wordId = this.$route.params.wordId;
-    },
     concatDataToHide() {
       store.state.partsOfSpeech.forEach((_, index) => {
         this.propsToHide.push(index + 1);
-      });
-    },
-    generateWordData() {
-      Object.keys(this.grammaticalCases).forEach((caseData) => {
-        this.grammaticalCasesArray.push(this.grammaticalCases[caseData].value);
-      });
-      Object.keys(this.selectedCard).forEach((key) => {
-        if (key === 'value') {
-          this.wordName = this.selectedCard[key];
-        }
-        if (key === 'translation') {
-          this.wordTranslation = this.selectedCard[key];
-        }
-        if (this.grammaticalCasesArray.includes(key)) {
-          this.wordTable.push(this.selectedCard[key]);
-        }
       });
     },
     createWordCases(counting) {
@@ -81,22 +59,7 @@ export default {
     },
   },
   created() {
-    this.getWordId();
-    store.state.languages[0].cards.forEach((card) => {
-      Object.keys(card).forEach((cardData) => {
-        if (card[cardData] === Number(this.wordId)) {
-          this.selectedCard = card;
-        }
-      });
-    });
-    this.generateWordData();
     this.createWordCases(this.counting);
-    this.concatDataToHide();
-  },
-  computed: {
-    grammaticalCases() {
-      return this.$store.state.grammaticalCases;
-    },
   },
 };
 </script>
--- lang-vue/src/components/WordView/WordView.vue
+++ lang-vue/src/components/WordView/WordView.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        <NounTable v-bind:counting="singular" />
-        <NounTable v-bind:counting="plural" />
+        <div class="word__name"><strong>{{ wordName }}</strong> - {{ wordTranslation }}</div>
+        <NounTable
+          v-bind:counting="singular"
+          v-bind:selectedCard="selectedCard"
+          v-bind:grammaticalCasesArray="grammaticalCasesArray"
+        />
+        <NounTable
+          v-bind:counting="plural"
+          v-bind:selectedCard="selectedCard"
+          v-bind:grammaticalCasesArray="grammaticalCasesArray"
+        />
     </div>
 </template>
 
 <script>
 import NounTable from './NounTable/NounTable.vue';
+import store from '../../store';
 
 export default {
   name: 'WordView',
@@ -17,7 +27,47 @@ export default {
     return {
       singular: 'lp',
       plural: 'lm',
+      selectedCard: {},
+      wordName: '',
+      wordTranslation: '',
+      wordId: null,
+      grammaticalCasesArray: [],
+      sharedState: store.state,
     };
+  },
+  methods: {
+    getWordId() {
+      this.wordId = this.$route.params.wordId;
+    },
+    generateWordData() {
+      Object.keys(this.grammaticalCases).forEach((caseData) => {
+        this.grammaticalCasesArray.push(this.grammaticalCases[caseData].value);
+      });
+      Object.keys(this.selectedCard).forEach((key) => {
+        if (key === 'value') {
+          this.wordName = this.selectedCard[key];
+        }
+        if (key === 'translation') {
+          this.wordTranslation = this.selectedCard[key];
+        }
+      });
+    },
+  },
+  created() {
+    this.getWordId();
+    store.state.languages[0].cards.forEach((card) => {
+      Object.keys(card).forEach((cardData) => {
+        if (card[cardData] === Number(this.wordId)) {
+          this.selectedCard = card;
+        }
+      });
+    });
+    this.generateWordData();
+  },
+  computed: {
+    grammaticalCases() {
+      return this.$store.state.grammaticalCases;
+    },
   },
 };
 </script>
