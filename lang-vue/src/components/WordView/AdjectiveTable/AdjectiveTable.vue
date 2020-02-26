6c3da9d88d54bb36124d1826607558982a59b81c
Tomek A <tomus.adamczyk@gmail.com>
2020-02-26 20:26:20 +0100

    Adjective word component

---
+++ lang-vue/src/components/WordView/AdjectiveTable/AdjectiveTable.vue
@@ -0,0 +1,68 @@
+<template>
+    <VTable
+        v-bind:tableDataArray="preparedData"
+        v-bind:checkRouter="checkRouter"
+        class="table"
+    />
+</template>
+
+<script>
+import VTable from '../../UI/Table/VTable.vue';
+
+export default {
+  name: 'AdjectiveTable',
+  components: {
+    VTable,
+  },
+  props: {
+    counting: {
+      type: String,
+    },
+    selectedCard: {
+      type: Object,
+    },
+    grammaticalCasesArray: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      checkRouter: false,
+      preparedData: [],
+    };
+  },
+  methods: {
+    createWordCases(counting) {
+      const newDictionaryObjects = {};
+      const arrayOfDictionaryObjects = [];
+      Object.keys(this.selectedCard).forEach((item) => {
+        if (typeof this.selectedCard[item] === 'object') {
+          Object.keys(this.selectedCard[item]).forEach((key) => {
+            if (key === counting) {
+              const singularGrammarCasesObject = this.selectedCard[item][key];
+              Object.keys(singularGrammarCasesObject).forEach((caseName) => {
+                newDictionaryObjects[item] = {
+                  case: caseName,
+                  masculine: singularGrammarCasesObject[caseName].masculine,
+                  feminine: singularGrammarCasesObject[caseName].feminine,
+                  neuter: singularGrammarCasesObject[caseName].neuter,
+                };
+                arrayOfDictionaryObjects.push(newDictionaryObjects[item]);
+              });
+            }
+          });
+        }
+      });
+      this.preparedData = arrayOfDictionaryObjects;
+      return this.preparedData;
+    },
+  },
+  created() {
+    this.createWordCases(this.counting);
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
