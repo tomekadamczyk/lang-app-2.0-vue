eaea2623a893619a677851dfa6d1c0c27a05c905
Tomek A <tomus.adamczyk@gmail.com>
2020-03-01 18:17:37 +0100

    apollo client implemented

--- lang-vue/src/main.js
+++ lang-vue/src/main.js
@@ -1,13 +1,38 @@
 import Vue from 'vue';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:4000/graphql',
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+Vue.use(VueApollo);
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app');
