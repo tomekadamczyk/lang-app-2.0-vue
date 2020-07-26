74c13046aa761322ec49029fb26869238c1abbe6
Tomek A <tomasz_adamczyk@hotmail.com>
2020-07-26 09:23:01 +0200

    getcookie function added to Dictionary

--- lang-vue/src/components/Dictionary/FetchedDictionary.vue
+++ lang-vue/src/components/Dictionary/FetchedDictionary.vue
@@ -44,7 +44,7 @@ export default {
         console.log(Number(this.$store.state.userId))
         return {
           langId: this.languageVarId,
-          userId: Number(this.$store.state.userId),
+          userId: Number(this.getCookie('userId')),
         };
       },
     },
@@ -64,6 +64,21 @@ export default {
       });
       this.preparedData = arrayOfDictionaryObjects;
       return this.preparedData;
+    },
+    getCookie(cname) {
+      const name = `${cname}=`;
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return '';
     },
   },
   created() {
